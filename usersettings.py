429908e722f82b3c0fd9e3b4f4ec40d81ed9f10b
Brady Snelson <bsnelson@umd.edu>
2021-03-06 18:40:33 -0500

    resume playback if same

--- usersettings.py
+++ usersettings.py
@@ -5,7 +5,7 @@
 sonoshttpaddress="http://localhost:5005"
 
 #set the name of the Sonos room you want to play the music in
-sonosroom="Bathroom"
+sonosroom=""
 
 #send anonymous usage statistics
 sendanonymoususagestatistics="yes"
